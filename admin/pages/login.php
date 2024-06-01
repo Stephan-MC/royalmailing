538b1d5a431532234cf992a4a7c1f9685b5fd573
Minister <mundestephane13@gmail.com>
2024-06-01 13:47:35 +0100

    updated meta tags in login page

--- admin/pages/login.php
+++ admin/pages/login.php
@@ -21,10 +21,9 @@ if(isset($_SESSION['loggedIn']) && $_SESSION['loggedIn'] == true){
 <html lang="en">
 
 
-<meta http-equiv="content-type" content="text/html;charset=UTF-8" />
 <head>
 
-    <meta charset="utf-8">
+    <meta http-equiv="content-type" content="text/html;charset=UTF-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="description" content="">
