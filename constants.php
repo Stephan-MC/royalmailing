0dc9b6e233da6888f069a2569e4b6edf217d742b
Minister <mundestephane13@gmail.com>
2024-06-01 13:49:10 +0100

    create file to store page constants

---
+++ constants.php
@@ -0,0 +1,7 @@
+<?php
+
+if (!defined('COMPANY_NAME'))
+  define('COMPANY_NAME', 'Global Express Agency');
+
+if (!defined('WEBSITE_URL'))
+  define('WEBSITE_URL', 'https://global-expressagency.com');
